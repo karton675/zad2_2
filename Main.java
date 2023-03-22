33c6ee0b285204b1119c850429d9ee8b2b8ce776
karton675 <127328589+karton675@users.noreply.github.com>
2023-03-22 15:59:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a=16;
+    int b=4;
+    int c = a+b;
+    System.out.println(c);
+    c = a - b;
+    System.out.println(c);
+    c = a * b;
+    System.out.println(c);
+    c = a / b;
+    System.out.println(c);
+    
+  }
+}
